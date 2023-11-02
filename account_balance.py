79a4a170bb30f89658de9930b09d985e1a5c4930
Qbits-manager1 <102313844+Qbits-manager1@users.noreply.github.com>
2023-11-02 16:33:47 +0530

    Create account_balance.py

---
+++ account_balance.py
@@ -0,0 +1,11 @@
+"""function to return the available balance in the account """
+def get_balance(acc_no):
+  # open the file in read mode
+  filename = open('balance.csv', 'r')
+  # create dictreader object to read the data from file
+  file = csv.DictReader(filename) 
+  # iterate over each row and append values to empty list
+  for col in file:       
+    if col['Account number'] == acc_no:
+      return float(col['Balance'])
+  return False 
